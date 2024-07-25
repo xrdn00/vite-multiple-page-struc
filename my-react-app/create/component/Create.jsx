093504d3d2228d002093bf670b39f5c9add0bb47
xrdn00 <reydan39@gmail.com>
2024-07-25 16:54:41 +0800

    create operation page

---
+++ my-react-app/create/component/Create.jsx
@@ -0,0 +1,9 @@
+import './Create.css'
+
+function Create() {
+    return (
+        <h1>Create Operation</h1>
+    );
+}
+
+export default Create;
--- my-react-app/nested/App.jsx
+++ my-react-app/nested/App.jsx
@@ -2,8 +2,6 @@ import './App.css';
 
 
 function App() {
-
-
   return (
     <h1>hello</h1>
   );
